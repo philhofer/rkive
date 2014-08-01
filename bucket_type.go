ce8d198e37a9b98605fb1a31fbd42877975edc69
philhofer <phofer@umich.edu>
2014-08-01 14:56:23 -0400

    bucket types - maybe?

--- bucket_type.go
+++ bucket_type.go
@@ -1,9 +1,26 @@
 package riakpb
 
 import (
-//"github.com/philhofer/riakpb/rpbc"
+	"github.com/philhofer/riakpb/rpbc"
 )
 
-type BucketType struct {
-	name []byte
+// GetBucketTypeProperties gets the bucket properties
+// associated with a given bucket type.
+// *NOTE* bucket types are a Riak 2.0 feature.
+func (c *Client) GetBucketTypeProperties(typeName string) (*rpbc.RpbBucketProps, error) {
+	req := &rpbc.RpbGetBucketTypeReq{}
+	req.Type = ustr(typeName)
+	res := &rpbc.RpbBucketProps{}
+	_, err := c.req(req, 31, res)
+	return res, err
+}
+
+// SetBucketTypeProperties sets the bucket properties
+// associated with a given bucket type.
+// *NOTE* bucket types are a Riak 2.0 feature.
+func (c *Client) SetBucketTypeProperties(typeName string, props *rpbc.RpbBucketProps) error {
+	req := &rpbc.RpbSetBucketReq{}
+	req.Props = props
+	_, err := c.req(req, 32, nil)
+	return err
 }
