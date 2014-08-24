40149978e63289712821c85dc4cad8f7dc611265
philhofer <phofer@umich.edu>
2014-08-24 19:30:35 -0400

    close *works* now

--- client_norace.go
+++ client_norace.go
@@ -2,12 +2,16 @@
 
 package rkive
 
+import "sync/atomic"
+
 // finish node (success)
 func (c *Client) done(n *conn) {
 	n.Close()
+	atomic.AddInt32(&c.inuse, -1)
 }
 
 // finish node (err)
 func (c *Client) err(n *conn) {
 	n.Close()
+	atomic.AddInt32(&c.inuse, -1)
 }
--- client_race.go
+++ client_race.go
@@ -1,25 +1,29 @@
 // +build !race
-
 package rkive
+
+import "sync/atomic"
 
 // finish node (success)
 func (c *Client) done(n *conn) {
 	if c.closed() {
 		n.Close()
-		return
+	} else {
+		c.pool.Put(n)
 	}
-	c.pool.Put(n)
+	atomic.AddInt32(&c.inuse, -1)
 }
 
 // finish node (err)
 func (c *Client) err(n *conn) {
 	if c.closed() {
 		n.Close()
-		return
+	} else {
+		err := ping(n)
+		if err != nil {
+			n.Close()
+		} else {
+			c.pool.Put(n)
+		}
 	}
-	err := ping(n)
-	if err != nil {
-		n.Close()
-	}
-	c.pool.Put(n)
+	atomic.AddInt32(&c.inuse, -1)
 }
