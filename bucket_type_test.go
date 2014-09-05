47d1a3c3a375bf30cebef4a1544b6bb0d76a1fdd
philhofer <phofer@umich.edu>
2014-09-05 13:24:32 -0400

    now implicitly covering GetProperties and SetProperties

--- bucket_type_test.go
+++ bucket_type_test.go
@@ -3,6 +3,7 @@
 package rkive
 
 import (
+	"bytes"
 	check "gopkg.in/check.v1"
 )
 
@@ -10,9 +11,20 @@ func (s *riakSuite) TestGetBucketTypeProperties(c *check.C) {
 	c.Skip("not implemented")
 }
 
-func (s *riakSuite) TestMakeCache(c *check.C) {
-	err := s.cl.Bucket("test-cache").MakeCache()
+func (s *riakSuite) TestCache(c *check.C) {
+	cache := s.cl.Bucket("test-cache")
+
+	err := cache.MakeCache()
 	if err != nil {
 		c.Fatal(err)
 	}
+
+	props, err := cache.GetProperties()
+	if err != nil {
+		c.Error(err)
+	}
+	if !bytes.Equal(props.GetBackend(), []byte("cache")) {
+		c.Errorf("Expected backend %q; got %q", "cache", props.GetBackend())
+	}
+
 }
